a5759f4d3ac1542e1ee486305efe1b3d092da7b2
sebastian <sebastian.ulbel@gmail.com>
2015-12-29 14:09:33 +0100

    Added Watch task

--- builds/development/js/script.js
+++ builds/development/js/script.js
@@ -77,7 +77,7 @@ $ = require('jquery');
 
 (fill = function(item) {
   return $('.tagline').append("" + item);
-})('The most creative minds in Art');
+})('The most Creative minds of Art');
 
 fill;
 
